193a2c4e765ca0e7bb743d57ef23c0c9c04b5c4a
victor neves <victorneves@weg.net>
2025-12-03 15:48:23 -0300

    setting up books route

--- src/app.js
+++ src/app.js
@@ -2,8 +2,26 @@ import express from "express";
 
 const app = express();
 
+const books = [
+    {
+        id: 1,
+        title: "Harry Potter",
+    },
+    {
+        id: 2,
+        title: "Lord of the Rings"
+    }
+]
+
+// Source route
 app.get("/", (req, res) => {
     res.status(200).send("Node Course");
 });
+
+// Books route
+app.get("/books", (req, res) => {
+    res.status(200).json(books);
+});
+
 
 export default app;
