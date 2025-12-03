b787d08392cbfd3b0ada0df02c13aadd9642b15e
victor neves <victorneves@weg.net>
2025-12-03 16:49:22 -0300

    added CRUD methods for books

--- src/app.js
+++ src/app.js
@@ -16,19 +16,45 @@ const books = [
     }
 ]
 
+function getBooks(id){
+    return books.findIndex(books => {
+        // parsing str => number for comparison
+        return books.id === Number(id);
+    });
+};
+
 // Source route
 app.get("/", (req, res) => {
     res.status(200).send("Node Course");
 });
 
-// Books route
+// Books routes
 app.get("/books", (req, res) => {
     res.status(200).json(books);
+});
+
+app.get("/books/:id", (req, res) => {
+    // params: (id) id: (name of parameter)
+    const index = getBooks(req.params.id);
+    res.status(200).json(books[index]);
 });
 
 app.post("/books", (req, res) => {
     books.push(req.body);
     res.status(201).send("Book registered successfully!");
 });
+
+app.put("/books/:id", (req, res) => {
+    const index = getBooks(req.params.id);
+    books[index].title = req.body.title;
+    res.status(200).json(books);
+});
+
+app.delete("/books/:id", (req, res) => {
+    const index = getBooks(req.params.id);
+    books.splice(index, 1);
+    res.status(200).send("Book removed successfully");
+});
+
 
 export default app;
