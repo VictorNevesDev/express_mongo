2a82802c7a23b19b1d4a19be2014c663de77c4f4
victor neves <victorneves@weg.net>
2025-12-03 15:40:28 -0300

    removed server routing and setting up express

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Node Course",
-    "/authors": "John" ,
-    "/books": "Science Book",
-};
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Server Listening ")
 });
