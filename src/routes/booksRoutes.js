86c970d8ede13584d33f931f8f2e0eeb3dfda27b
victor neves <victorneves@weg.net>
2025-12-05 11:27:19 -0300

    improving CRUD

--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -4,8 +4,8 @@ import BooksController from "../controllers/booksController.js";
 const routes = express.Router();
 
 routes.get("/books", BooksController.getBooks);
-routes.get("/books/:id", BooksController.getBookById);
 routes.post("/books", BooksController.postBooks);
+routes.get("/books/:id", BooksController.getBookById);
 routes.put("/books/:id", BooksController.updateBookById);
 routes.delete("/books/:id", BooksController.deleteBookById);
 
